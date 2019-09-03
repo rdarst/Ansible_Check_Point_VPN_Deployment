facbf1dac4c5aadc67c27adac2d7ab2beadc793f
rdarst <45427115+rdarst@users.noreply.github.com>
2019-09-03 14:07:48 -0400

    Adding primary VPN files

---
+++ primary/build_primary_vpn.sh
@@ -0,0 +1,2 @@
+ansible-playbook add_vpn.yml --extra-vars "community_name=Ansible_VPN_Demo vpn_subnet_name=vpn_subnet vpn_subnet=192.168.2.0 vpn_subnet_length=24 interoperable_name=r80dot30dlp interoperable_ip=10.2.0.189 local_gateway=CPX_API_GW policy_package=CheckMates_Demo_Policy"
+
